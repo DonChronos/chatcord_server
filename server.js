5fa866866b65213a97f7626aaba3fdb8b3b6e5b2
John <johndoemisc@tutamail.com>
2020-06-23 17:57:53 +0300

    Chatcord bot successfully notifies users of important info

--- server.js
+++ server.js
@@ -15,14 +15,44 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const botName = 'Chatcord Bot';
+
 io.on('connection', socket => {
   console.log('New ws connection...');
+  socket.on('joinRoom', ({ userId, activeRoom }) => {
+    const user = userJoin(socket.id, userId, activeRoom);
+    console.log(user);
+    socket.join(user.room);
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatcord!'));
+    socket.broadcast.to(user.room).emit(
+      'message',
+     formatMessage(botName, `${user.username} has joined the chat`));
 
+     io.to(user.room).emit('roomUsers', {
+       room: user.room,
+       users: getRoomUsers(user.room)
+     })
+  });
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+    )
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
     console.log('a user disconnected');
-  })
-})
+  });
+});
 
 const PORT = 3000 || process.env.PORT;
 
