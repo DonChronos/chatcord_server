238931202b2d59cc3d82b92ddfcd73156cad75e0
John <johndoemisc@tutamail.com>
2020-06-23 15:11:50 +0300

    Added utils

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+const rooms = ['HTML', 'CSS', 'JAVASCRIPT'];
 
 const app = express();
 const server = http.createServer(app);
@@ -8,7 +17,11 @@ const io = socketio(server);
 
 io.on('connection', socket => {
   console.log('New ws connection...');
-  socket.emit('message', 'Welcome to Chatcord!');
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+    console.log('a user disconnected');
+  })
 })
 
 const PORT = 3000 || process.env.PORT;
